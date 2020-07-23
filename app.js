0b98384e670a254a0bcdc79e89fe525ab02aa20b
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-07-23 16:33:04 -0400

    Start/stop animation

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 function flip() { document.getElementById('card').className = 'card flipped'; }
 function unflip() { document.getElementById('card').className = 'card'; }
+//Start or Stop the animation
+const stopButtonElement = document.getElementById("stop-btn")
+stopButtonElement.addEventListener("click", function () {
+  stopButtonElement.style.backgroundColor = (started) ? "green" : "red";
+  stopButtonElement.style.borderBottomColor = (started) ? "darkgreen" :"darkred"
+  started = !started;
+
+})
